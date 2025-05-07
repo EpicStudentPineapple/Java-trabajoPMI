5ae9403a088e9c6d31daae641f844a78327e4902
javier <ikcec@plaiaundi.net>
2025-05-07 12:37:52 +0200

    1era vista

--- PMI_Reto/src/controller/ControllerLogin.java
+++ PMI_Reto/src/controller/ControllerLogin.java
@@ -6,4 +6,10 @@ public class ControllerLogin {
 	
 	private VistaLogin vista;
 	
+	public  ControllerLogin(VistaLogin vista) {
+        this.vista = vista;}
+	
+	public void iniciar() {
+        vista.setVisible(true);
+    }
 }
